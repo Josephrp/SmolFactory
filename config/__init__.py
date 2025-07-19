58a74d273ce896c8ca95e3965f5670de5920911b
Tonic <tonic@tonic-ai.com>
2025-07-20 00:36:54 +0200

    adds max perf script

--- config/__init__.py
+++ config/__init__.py
@@ -6,6 +6,7 @@ from .train_smollm3 import SmolLM3Config, get_config as get_base_config
 from .train_smollm3_openhermes_fr import SmolLM3ConfigOpenHermesFR, get_config as get_openhermes_fr_config
 from .train_smollm3_openhermes_fr_a100_large import SmolLM3ConfigOpenHermesFRA100Large, get_config as get_a100_large_config
 from .train_smollm3_openhermes_fr_a100_multiple_passes import SmolLM3ConfigOpenHermesFRMultiplePasses, get_config as get_multiple_passes_config
+from .train_smollm3_openhermes_fr_a100_max_performance import SmolLM3ConfigOpenHermesFRMaxPerformance, get_config as get_max_performance_config
 
 # Generic get_config function that can handle different config types
 def get_config(config_path: str):
@@ -20,6 +21,8 @@ def get_config(config_path: str):
         return get_a100_large_config(config_path)
     elif "a100_multiple_passes" in config_path:
         return get_multiple_passes_config(config_path)
+    elif "a100_max_performance" in config_path:
+        return get_max_performance_config(config_path)
     elif "openhermes_fr" in config_path:
         return get_openhermes_fr_config(config_path)
     else:
@@ -30,9 +33,11 @@ __all__ = [
     'SmolLM3ConfigOpenHermesFR', 
     'SmolLM3ConfigOpenHermesFRA100Large',
     'SmolLM3ConfigOpenHermesFRMultiplePasses',
+    'SmolLM3ConfigOpenHermesFRMaxPerformance',
     'get_config',
     'get_base_config',
     'get_openhermes_fr_config',
     'get_a100_large_config',
     'get_multiple_passes_config',
+    'get_max_performance_config',
 ] 
